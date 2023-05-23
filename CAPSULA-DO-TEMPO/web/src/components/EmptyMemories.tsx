9de22a9c8b18305fb4d3a684a5376df38191e59a
Yan Caue <coldecmastercrelan@gmail.com>
2023-05-23 16:30:41 -0300

    novos arquivos

---
+++ CAPSULA-DO-TEMPO/web/src/components/EmptyMemories.tsx
@@ -0,0 +1,12 @@
+export function EmptyMemories() {
+  return (
+    <div className="flex flex-1 items-center justify-center">
+      <p className="w-[360px] text-center leading-relaxed">
+        Você ainda não registrou nenhuma lembrança, comece a{' '}
+        <a href="" className="houver:text-gray-50 underline">
+          criar agora!
+        </a>
+      </p>
+    </div>
+  )
+}
