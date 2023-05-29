6338dd3928b918d7a2ca987b8632818edfbc1e32
Yan Caue <coldecmastercrelan@gmail.com>
2023-05-29 14:56:29 -0300

    novos arquivos

--- CAPSULA-DO-TEMPO/server/src/routes/upload.ts
+++ CAPSULA-DO-TEMPO/server/src/routes/upload.ts
@@ -38,5 +38,7 @@ export async function uploadRoutes(app: FastifyInstance) {
 
     const fullUrl = request.protocol.concat('://').concat(request.hostname)
     const fileUrl = new URL(`/uploads/${fileName}`, fullUrl).toString()
+
+    return { fileUrl }
   })
 }
